3ea08e205a9dc58f87209f742e5fd470ee0d1d0c
clragon <kenta3ds.habbo@gmail.com>
2019-04-25 13:50:35 +0200

    updated docstrings

--- driver.py
+++ driver.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-# libraries for calculation and waiting
+# library for calculations
 import math
-import time
-import sys
 
 class driver(object):
+    '''A class to control an EV3 robot object.
+    
+    Supports moving inside a coordinate system.
+    All coordniates are saved, so the robot can retreat to the starting point later.
+    '''
 
     # default conditions: robot is facing right and is at pos 0,0 at the bottom left of the coordinates system.
 
@@ -16,7 +19,11 @@ class driver(object):
 
 
     def __init__(self, vehicle):
+        '''Instantiate a new driver object.
 
+        Parameters:
+            vehicle (obj): A robot object the driver will control.
+        '''
         self.robot = vehicle
 
         # array to store past coordinates
@@ -27,7 +34,15 @@ class driver(object):
 
 
     # return angle to turn by for new coordinates
-    def calc_angle(self, x, y):
+    def _calc_angle(self, x, y):
+        '''Intern function. Do not call this from outside.
+        
+        Calculate the angle at which to turn by to face the new coordniates.
+
+        Parameters:
+            x (int): The new x coordinate.
+            y (int): The new y coordinate.
+        '''
         if (x == 0):
             if (y < 0):
                 return -90
@@ -39,12 +54,24 @@ class driver(object):
 
 
     # return distance to new coordinates
-    def calc_hypo(self, x, y):
+    def _calc_hypo(self, x, y):
+        '''Intern function. Do not call this from outside.
+        
+        Calculate the distance to the new coordinates.
+
+        Parameters:
+            x (int): The new x coordinate.
+            y (int): The new y coordinate.
+        '''
         return math.sqrt(((x**2)+(y**2)))
 
 
     # unpoint the robot
-    def unpoint(self):
+    def _unpoint(self):
+        '''Intern function. Do not call this from outside.
+        
+        Revert the robot from pointing to standing on the target coordinates.
+        '''
 
         if (self.pointing):
 
@@ -58,8 +85,16 @@ class driver(object):
 
     # move to new coordinates
     def move(self, x_target, y_target, point=False, logging=True):
+        '''Move the robot to new coordinates.
 
-        self.unpoint()
+        Parameters:
+            x_target (int): The target x coordinates.
+            y_target (int): The target y coordinates.
+            point (bool): Wether the robot should point at the new coordinates or not. False by default.
+            logging (bool): Wether the robot should log the new position for when retreating. True by default.
+        '''
+
+        self._unpoint()
 
         # calculating how much we have to turn and move forward with default conditions
 
@@ -71,10 +106,10 @@ class driver(object):
         self.a_source = 0
 
         # angle we have to turn by
-        angle = self.calc_angle(x_diff, y_diff)
+        angle = self._calc_angle(x_diff, y_diff)
 
         # length we have to move by
-        hypo = self.calc_hypo(x_diff, y_diff)  
+        hypo = self._calc_hypo(x_diff, y_diff)  
 
         # set pointer variable for next run
         self.pointing = point
@@ -97,14 +132,17 @@ class driver(object):
 
         # log movement
         if (logging):
-            mov = [self.x_source, self.y_source]
-            self.log.append(mov)
+            log_cords(self.x_source, self.y_source)
 
 
     def retreat(self):
+        '''Retreate the robot.
+        
+        Will move back over all coordinates that have been logged, ending with the first.
+        '''
 
         # unpoint, so the calculation fits again
-        self.unpoint()
+        self._unpoint()
         self.log.reverse()
 
         # move back to every coordinate that was visited
@@ -114,3 +152,15 @@ class driver(object):
         # revert angle at which the turtle rests
         self.robot.turn(self.a_source)
         self.a_source = 0
+
+    def log_cords(self, x_new, y_new):
+        '''Appends coordinates to the log.
+        
+        You should call this with 0, 0 when instantiating a new driver, 
+        to ensure the robot retreating all the way back.
+
+        Parameters:
+            x_new (int): The x coordinate to be logged.
+            y_new (int): The y coordinate to be logged.
+        '''
+        self.log.append([x_new, y_new])
