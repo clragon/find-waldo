c4578563aac1e7eac353503ab40f865b207769f5
clragon <kenta3ds.habbo@gmail.com>
2019-04-25 13:09:40 +0200

    updated comments

--- modules/remote_robot.py
+++ modules/remote_robot.py
@@ -1,66 +1,58 @@
 #!/usr/bin/env python3
 
-# libraries for calculations
+# library for calculations (turning degrees, etc).
 import math
-import time
 
-# library for remote connection
+# library for remote connection to the robot.
 import rpyc
-
-# Import all libraries necessary to run ev3
-# obsolete. we import this remotely now.
-# TODO: remove line
-# from ev3dev.ev3 import Sound, LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
 
 class Robot(object):    
 
-    # default motor speed value
+    # default motor speed value (tc/s).
     base_speed = 400
 
-    # default motor ramping up/down value (ms)
+    # default motor ramping up/down value (ms).
     base_ramp=600
 
 
-    # radius of a wheel (mm)
+    # radius of a wheel (mm).
     radius = 15
 
-    # diameter between the two wheels (mm)
+    # diameter between the two wheels (mm).
     diameter = 140
 
     
-    # pointer length (mm)
+    # pointer length (mm).
     pointer = 160
 
 
     def __init__(self, adress):
 
-        self.remote = rpyc.classic.connect(adress) # TODO: get EV3 hostname / IP adress for this field
+        # establish a connection to the robot at the given IP.
+        self.remote = rpyc.classic.connect(adress)
 
-        # remote import of the ev3dev library
+        # instantiate the EV3 dev module on the robot.
         self.ev3 = self.remote.modules['ev3dev.ev3']
 
-        # create objects for left motor (output B) and right motor (output C) as well as a pointer motor (output A)
-        # create these objects over RPyC
+        # create motor control objects with the remote EV3 dev module.
         self.mP = self.ev3.MediumMotor('outA'); self.mP.stop_action = 'hold'
         self.mL = self.ev3.LargeMotor('outB'); self.mL.stop_action = 'hold'
         self.mR = self.ev3.LargeMotor('outC'); self.mR.stop_action = 'hold'
 
-        # no need to do calculations remotely (?).
-
-        # calcultaing the circumfence (mm)
+        # calcultaing the circumfence (mm) of a wheel.
         self.circ = 2 * math.pi * self.radius
 
-        # degrees for one mm
+        # caculating how many degrees a wheel has to turn to move one mm.
         self.one_mm = 1 / (self.circ / 360)
 
-        # circumfence of the turning circle of both wheels
+        # calculating the circumfence of the turning circle of the robot.
         self.rob_circ = 2 * math.pi * (self.diameter / 2)
 
-        # how much degrees both wheels have to turn to turn the entire robot for 1 degree
+        # calculating how much degrees both wheels have to turn in order for the robot to turn one degree.
         self.turn_deg = 360 / self.circ * ( self.rob_circ / 360)
 
 
-    # go x mm straight.
+    # drive straight for the given amount of mm. Optionally, speed and ramping can ge passed as parameters.
     def drive(self, mm, anfahren=base_ramp, bremsen=base_ramp):
         self.mL.run_to_rel_pos(position_sp=mm * self.one_mm, speed_sp=self.base_speed, ramp_up_sp=anfahren, ramp_down_sp=bremsen)
         self.mR.run_to_rel_pos(position_sp=mm * self.one_mm, speed_sp=self.base_speed, ramp_up_sp=anfahren, ramp_down_sp=bremsen)
@@ -68,7 +60,7 @@ class Robot(object):
         self.mL.wait_while('running')
             
 
-    # turn right by x degrees
+    # turn the robot to the right by given degrees. Minus degrees can be given to turn to the left.
     def turn(self, degrees):
         self.mL.run_to_rel_pos(position_sp=-degrees * self.turn_deg, speed_sp=self.base_speed)
         self.mR.run_to_rel_pos(position_sp=+degrees * self.turn_deg, speed_sp=self.base_speed)
@@ -76,12 +68,12 @@ class Robot(object):
         self.mR.wait_while('running')
 
 
-    # robot text to speak; now remotely.
+    # let the robot speak the given text.
     def speak(self, text):
         self.ev3.Sound.speak(text)
 
 
-    # point for x ms
+    # raise or lower the pointer of the robot.
     def point(self, do_point):
         if (do_point):
             self.mP.run_to_abs_pos(position_sp=90, speed_sp=base_speed)
