061742e7c15f8d07f3058c2a9e9976ca5f2e9e30
walter <walter@TIM-N418.ti8m.ch>
2019-04-25 14:24:38 +0200

    Call brain and some error handling

--- find_waldo.py
+++ find_waldo.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from modules.remote_robot import Robot
 from brain import Brain
 from modules.cam import Camera
-from modules.image import Image
 from cam_config import CAMERA_URL
 import numpy as np
 from PIL import Image as PilImage
@@ -15,6 +14,16 @@ def find_waldo():
     print("Taking a picture...")
     cam = Camera()
     img = cam.take_snapshot(CAMERA_URL)
+    if img is None:
+        exit("No image recorde")
+
+    print("Rec Waldo...")    
+    brain = Brain(False, "model2_10200epochs.h5")
+    result = brain.find_waldo(img)
+    if result is None:
+        exit("Waldo not found")
+
+    print("Result from Brain: {}".format(result))
 
     print("Initialize Robot...")
     robot = Robot("192.168.137.43")
