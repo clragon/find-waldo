5d3fb7754ea92eaa67ca44387042df0ddf8b3714
Michele Falzone <michelefalzone@ti8m.ch>
2019-05-20 17:32:22 +0200

    Work in progress: TensorFlow doesn't return values

--- find_waldo.py
+++ find_waldo.py
@@ -11,8 +11,7 @@ import numpy as np
 from PIL import Image as PilImage
 from config import *
 
-
-def find_waldo():
+def searchWaldo():
     print("start finding waldo...")  
     image_height = 280
 
@@ -23,13 +22,13 @@ def find_waldo():
     #    exit("No image recorde")
 
     print("load image from folder...") 
-    img = PilImage.open('photo01.png')
-    img.show()
+    img = PilImage.open('docs/last_photo.jpg')
+    # img.show()
 
     print("Rec Waldo...")    
     brain = Brain(False, "model2_10200epochs.h5")
     result = brain.find_waldo(img)
-    #result = (400, 120)
+    result = (400, 120)
     if result is None:
         exit("Waldo not found")
     x,y = result
@@ -43,20 +42,33 @@ def find_waldo():
     # Currently the robot drives backwards, reverse direction
     y = -y
     x = -x
-    
+    return (20,20)
+
+def searchFakeWaldo():
+    return (20,20)
+
+def moveToWaldo():
     # Initialize a robot and a driver for it.
     print("Initialize Robot...")
     robot = Robot(IP_ADDRESS)
-
+    print("DEBUG")
     driver = Driver(robot)
-
     # tell the driver to move to new coordniates.
+    x=20
+    y=20
     print("Move robot to coordinates: {} {}".format(x,y))
     driver.move(x, y, True)
     driver.speak('I am pointing')
     # tell the driver to drive back to it's original location.
     driver.retreat()
 
+def find_waldo():
+    (x,y) = searchWaldo()
+    
+    print("Waldo coordinates")
+    print("X = " + str(x))
+    print("Y = " + str(y))
+    # moveToWaldo()
 
 if __name__ == '__main__':
     find_waldo()
--- modules/cam.py
+++ modules/cam.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import urllib.request
+import requests
 import numpy as np
 from PIL import Image as PilImage
 
@@ -13,7 +13,7 @@ class Camera():
         print ("take_snapshot")
         # Use urllib to get the image from the IP camera
         urllib.request.urlretrieve(url, 'last_photo.jpg')
-        img = np.array(PilImage.open('last_photo.jpg').rotate(270, expand=True))
+        img = np.array(PilImage.open('docs/last_photo.jpg').rotate(270, expand=True))
 
         return Image(data=img, width_in_mm=REAL_IMAGE_WIDTH_IN_MM, height_in_mm=REAL_IMAGE_HEIGHT_IN_MM)
 
