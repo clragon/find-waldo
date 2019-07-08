7158f26ae960d7da36b87d3ed0dae418b332cca3
waldo <Waldo@TIM-N418.ti8m.ch>
2019-07-08 12:48:42 +0200

    updated main

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from robot import *
 from image import *
 
-
-Pixel = Finde_Person("docs/Fotos/Person.jpg", "docs/Fotos/Gruppe.jpg")
-Koordinaten = Konvertieren(Pixel, Vergrössern("docs/Fotos/Gruppe.jpg", 1000))
-
+print("Bereiten sie die Webcam vor.")
+print("Als Gruppe vor die Kamera stehen, dann enter drücken")
+input()
+Gruppe = Foto()
+print("Einzelne Person vor Kamera stellen, enter drücken")
+input()
+Person = Foto()
+print("Das Gruppenfoto ausdrucken (Datei hier: {}) und die Breite in cm eingeben")
+Breite = int(input())
+print("Roboter bereitstellen, dann enter drücken")
+input()
+print("Finde Person...")
+Pixel = Finde_Person(Person, Gruppe)
+Koordinaten = Konvertieren(Pixel, Vergrössern(Gruppe, Breite))
+print("Fahre Roboter")
 Strecke, Winkel = Hypotenuse2(Koordinaten)
 Links(Winkel)
 Vorwärts(Strecke - ROBOT_ARM_SIZE)
+Zeigen()
+Sprich("Person gefunden")
 
